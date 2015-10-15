b40f1646315b9103ee494ead9dfde86ca190eb53
Valdensis <david.kcam@gmail.com>
2015-10-15 22:32:20 +0200

    MainActivity 1re version

--- app/src/main/java/com/androidprojects/inventaireii/MainActivity.java
+++ app/src/main/java/com/androidprojects/inventaireii/MainActivity.java
@@ -4,14 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Données provisoires :
+    boolean inventoryIsRunning = true;
+    int nbItems = 56;
+    int nbInventoredItems = 18;
+
+    // Declarations
+    TextView txtInventoryRunning;
+    TextView txtInventoryState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this site;
+
+        txtInventoryRunning = (TextView) findViewById(R.id.txtInventoryRunning);
+        txtInventoryState = (TextView) findViewById(R.id.txtInventoryState);
+        if (inventoryIsRunning) {
+            txtInventoryRunning.setVisibility(View.VISIBLE);
+            txtInventoryState.setText("Avancement de votre inventaire : "
+                    + nbInventoredItems + "/" + nbItems);
+            txtInventoryState.setVisibility(View.VISIBLE);
+        }
+        else {
+            txtInventoryRunning.setVisibility(View.INVISIBLE);
+            txtInventoryState.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
