df1f32f8ba076be41ef01b7610c543d4c5cfd1b6
Valdensis <david.kcam@gmail.com>
2015-10-22 18:53:56 +0200

    Fenêtre pop-up de confirmation pour le démarrage de l'inventaire

--- app/src/main/java/com/androidprojects/inventaireii/MainActivity.java
+++ app/src/main/java/com/androidprojects/inventaireii/MainActivity.java
@@ -2,9 +2,13 @@ package com.androidprojects.inventaireii;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     // Declarations
     TextView txtInventoryRunning;
     TextView txtInventoryState;
+    PopupWindow popupWindow;
+    Button buttonStartInventory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,51 @@ public class MainActivity extends AppCompatActivity {
             txtInventoryRunning.setVisibility(View.INVISIBLE);
             txtInventoryState.setVisibility(View.INVISIBLE);
         }
+
+        // Pop-up Window on click on 'New Inventory'
+        buttonStartInventory = (Button) findViewById(R.id.buttonStartInventory);
+        buttonStartInventory.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // Create the pop-up window
+                LayoutInflater layoutInflater =
+                        (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView =
+                        layoutInflater.inflate(R.layout.activity_activity_popup_ok_cancel, null);
+                popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.MATCH_PARENT);
+
+                // Catch the elements of the pop-up view
+                TextView txtQuestion = (TextView) popupView.findViewById(R.id.txtQuestion);
+                Button buttonOk = (Button) popupView.findViewById(R.id.buttonOk);
+                Button buttonCancel = (Button) popupView.findViewById(R.id.buttonCancel);
+
+                // Set actions to the elements
+                txtQuestion.setText("Êtes-vous sûr de vouloir lancer l'inventaire ?\n\n" +
+                        "Si oui le processus d'inventaire sera remis à zéro et tous vos" +
+                        "produits apparaîtront comme étant non inventoriés.");
+
+                buttonOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        restartInventory();
+                        popupWindow.dismiss();
+                    }
+                });
+
+                buttonCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+                popupWindow.showAsDropDown(buttonStartInventory, 0, -100);
+
+            }
+        });
     }
 
     @Override
@@ -56,5 +107,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void restartInventory() {
+        // TODO
+        nbInventoredItems = 0;
+        txtInventoryState.setText("Avancement de votre inventaire : "
+                + nbInventoredItems + "/" + nbItems);
+        txtInventoryState.setVisibility(View.VISIBLE);
+
     }
 }
